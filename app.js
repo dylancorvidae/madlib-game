0e47ebbfe60e38003067451d1a72cf5e926e59d1
Dylan Corvidae <dylancorvidae@gmail.com>
2019-07-10 21:05:46 -0700

    not done madlib

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const noun1 = document.getElementById('noun1');
+const noun2 = document.getElementById('noun2');
+const noun3 = document.getElementById('noun3');
+const noun4 = document.getElementById('noun4');
+const noun5 = document.getElementById('noun5');
+const noun6 = document.getElementById('noun6');
+const verbed1 = document.getElementById('verbed1');
+const verbed2 = document.getElementById('verbed2');
+const verb = document.getElementById('verb');
+const verbing = document.getElementById('verbing');
+const adjective1 = document.getElementById('adjective1');
+const adjective2 = document.getElementById('adjective2');
+
+Function updatenoun1(){
+    noun1.textContent = noun1Input.value
+}
